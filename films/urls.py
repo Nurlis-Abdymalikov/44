e3f44a4e03bb88b82aae87b2bb822c846428989a
Nurlis-Abdymalikov <Nurlisabdymalikov0@gmail.com>
2025-05-15 20:43:39 +0600

    hw4

--- films/urls.py
+++ films/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('films/', views.films_list_view, name='films'),
     path('films/<int:id>/', views.film_detail_view, name='film_detail'),  
+    path('films/<int:id>/delete/', views.delete_film_view, name='delete_film'),  
+    path('films/<int:id>/update/', views.update_film_view, name='update_film'),
+    path('create_film/', views.create_film_view, name='create_film'),  
 ]
--- films/views.py
+++ films/views.py
@@ -1,5 +1,42 @@
-from django.shortcuts import render, get_object_or_404
-from . import models
+from django.shortcuts import render, get_object_or_404, redirect
+from . import models, forms
+from django.http import HttpResponse
+
+
+#update film
+def update_film_view(request,id):
+    film_id = get_object_or_404(models.Film, id=id)
+    if request.method == "POST":
+        form  = forms.FilmForm(request.POST, instance=film_id)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Книга успешно обновлена')
+    else:
+        form = forms.FilmForm(instance=film_id)
+    return render(request, template_name='films/update_film.html', context={
+        'form': form,
+        'film_id': film_id,
+        })
+
+
+# Delete film
+def delete_film_view(request, id):
+    film_id = get_object_or_404(models.Film, id=id)
+    film_id.delete()
+    return HttpResponse('Книга успешно удалена')
+
+
+
+#create_book
+def create_film_view(request):
+    if request.method == 'POST':
+        form = forms.FilmForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Фильм успешно добавлена')
+    else:
+        form = forms.FilmForm()
+    return render(request, template_name='films/create_film.html', context={'form': form})
 
 def films_list_view(request):
     if request.method == 'GET':
