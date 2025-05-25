3fe1689ffe002899e47d5df0af98d100a209e22d
Nurlis-Abdymalikov <Nurlisabdymalikov0@gmail.com>
2025-05-25 14:43:04 +0600

    hw6again

--- parser_app/parser_movie.py
+++ parser_app/parser_movie.py
@@ -15,28 +15,29 @@ def get_html(url, params=''):
 
 def get_data(html):
     bs = BS(html, features='html.parser')
-    items = bs.find_all("div", class_='middle-content-wr clearfix')
+    items = bs.find_all("div", class_='custom1-item')
     movie_list = []
 
     for item in items:
-        title_tag = item.find("div", class_='custom1-title')
-        if title_tag:
-            title = title_tag.get_text(strip=True)
+        title_block = item.find("div", class_='custom1-title')
+        if title_block:
+            title = title_block.get_text(strip=True)
             movie_list.append({'title': title})
-
-    return movie_list
     
+    return movie_list
+
+
 def parsing_movie():
     response = get_html(URL)
     if response.status_code == 200:
         movie_list_2 = []
-        for page in range(1, 3):
-            response = get_html('https://kinovibe.co/genreserial/boevik/page/2/', params={'page':page})
+        for page in range(1, 2):
+            response = get_html('https://kinovibe.co/genreserial/boevik/', params={'page':page})
             movie_list_2.extend(get_data(response.text))
         return movie_list_2
     else:
         raise Exception('error in parse')
 
-# if __name__ == '__main__':
-#     print(parsing_movie())
+if __name__ == '__main__':
+    print(parsing_movie())
 
